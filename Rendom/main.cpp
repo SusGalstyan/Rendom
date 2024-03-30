097f690cdd32a49c521d1d145557093c4974accc
Admin <Admin@LAPTOP-G4NUK9IK>
2024-03-30 03:10:18 +0300

    Add project files.

---
+++ Rendom/main.cpp
@@ -0,0 +1,23 @@
+Random
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+void main()
+{
+	setlocal(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+
+	//заполнение масива случайными числами:
+	for (int i = ; i < n; i++)
+	{
+		arr[i] = rand() % 100 + 50;   //
+	}
+
+	//вывод массива на экран:
+
+}
